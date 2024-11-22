0f476af4f32bc63a552526dcdde78b1224eb6fba
claire-stullenbarger <claire.stullenbarger@mykenzie.snhu.edu>
2024-11-22 12:11:04 -0500

    initial

---
+++ src/main/java/com/example/coinzipp/CoinzippApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinzipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinzippApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinzippApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coinzipp/CoinzippApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinzipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinzippApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
